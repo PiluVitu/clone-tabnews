f9961644ded2c8f9ba7c376fb4e592a8be0d3b1e
Paulo Victor <Paulo.tspi@gmail.com>
2023-06-07 16:51:43 +0000

    Corrrigindo erros

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,13 @@ import Image from "next/image";
 function Home(){
   return (
     <div>
-      <h1>Mozao </h1>
-      <h2>No meu pedacinho de internet</h2>
+      <h1>Mozao&hearts;</h1>
+      <h2>&hearts</h2>
+      
       <p>Estou construindo esse pedacinho de internet 
         e gostaria de te compartilhar um pedaço dele, 
         por enquanto está simples, porque estou seguindo 
         o fluxo do curso, mas é de coração &hearts;</p>
-      <img src="https://imgur.com/eJmN9xa" alt="capybaraValentine"/>
     </div>
   )
 }
