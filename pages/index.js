13becf530a9cf883b13a9602f2a332f77b528edd
Paulo Victor <Paulo.tspi@gmail.com>
2023-06-08 15:21:40 +0000

    Add new emote and image on site para my mozão

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,13 @@
-import Image from "next/image";
-
 function Home(){
   return (
     <div>
-      <h1>Mozao&hearts;</h1>
-      <h2>&hearts</h2>
+      <h1>Mozao eu te amoo &hearts;</h1>
       
       <p>Estou construindo esse pedacinho de internet 
         e gostaria de te compartilhar um pedaço dele, 
         por enquanto está simples, porque estou seguindo 
-        o fluxo do curso, mas é de coração &hearts;</p>
+        o fluxo do curso, mas é de coração &hearts;🦊</p>
+      <img src="https://i.redd.it/6vhkra8p2uha1.jpg" alt="Capybara Love"/>
     </div>
   )
 }
