2dda506b1f823504fa3d5141323e244088fb47a6
Paulo Victor <Paulo.tspi@gmail.com>
2023-06-23 16:42:01 +0000

    adiciona scripts `lint:check` e`lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Clone do TabNews
+
 Clone do tabnews feito com base nas aulas do [curso.dev](https://curso.dev), ministrado pelo [@filipedeschamps](https://github.com/filipedeschamps)
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
-function Home(){
+function Home() {
   return (
-      <div>
+    <div>
       <h1>Mozao eu te amoo &hearts;</h1>
-      
-      <p>Estou construindo esse pedacinho de internet 
-        e gostaria de te compartilhar um pedaço dele, 
-        por enquanto está simples, porque estou seguindo 
-        o fluxo do curso, mas é de coração &hearts;🦊</p>
-      <img src="https://i.redd.it/6vhkra8p2uha1.jpg" alt="Capybara Love"/>
+
+      <p>
+        Estou construindo esse pedacinho de internet e gostaria de te
+        compartilhar um pedaço dele, por enquanto está simples, porque estou
+        seguindo o fluxo do curso, mas é de coração &hearts;🦊
+      </p>
+      <img src="https://i.redd.it/6vhkra8p2uha1.jpg" alt="Capybara Love" />
     </div>
-  )
+  );
 }
 
 export default Home;
