9cea24bcd2f103bc82c2fd27a386fe7debecba99
Paulo Victor <Paulo.tspi@gmail.com>
2023-08-05 15:22:00 +0000

    add`mozao.js` para preservar a pag feita ateriormente

---
+++ pages/mozao.js
@@ -0,0 +1,14 @@
+export default function Mozao() {
+  return (
+    <>
+      <h1>Mozao eu te amoo &hearts;</h1>
+
+      <p>
+        Estou construindo esse pedacinho de internet e gostaria de te
+        compartilhar um pedaço dele, por enquanto está simples, porque estou
+        seguindo o fluxo do curso, mas é de coração &hearts;🦊
+      </p>
+      <img src="https://i.redd.it/6vhkra8p2uha1.jpg" alt="Capybara Love" />
+    </>
+  );
+}
